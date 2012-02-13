6dabf1ee21943f99061912fcb3a2c1b4a4dc95ea
Frederic Leroux <frederic@leroux.im>
2012-02-13 20:20:04 +0100

    Test

--- sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash          
-# Github Syncro Script
-
-git pull
-git add --all
-git commit -m "Auto-Git-Backup $(date "+%d.%m.%Y %H:%M "|sed -e ' s/\"/\\\"/g' )"
-git push -u origin master
